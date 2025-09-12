0e11e99bf97256e3533e0fc165e2a097d91262e5
idk3777 <aroslavn924@gmail.com>
2025-09-12 13:14:18 +0300

    12.09
    кл раб и дз

---
+++ pythonProject/123.py
@@ -0,0 +1,43 @@
+'''
+ #1
+a = int(input("Введите число: "))
+if a % 13 == 0 :
+     print("делится")
+else:
+     print("не делится")
+#2
+b = int(input("Введите 3-х значное число: "))
+print(b % 10)
+b = b // 10
+print(b % 10)
+b = b // 10
+print(b % 10)
+b = b // 10
+#3
+
+a = int(input("Введите четырех значное число: "))
+b = (a % 10)
+a = a // 10
+c = (a % 10)
+a = a // 10
+d = (a % 10)
+a = a // 10
+e = (a % 10)
+print("Cумма всех цифер:", c+b+d+e)
+'''
+age = int(input("Введите ваш возраст для входа в бар:"))
+if 0 < age < 18 :
+      print("Вам еще рано")
+else:
+    print("Проходите")
+a = int(input("Введите число: "))
+print(a * 1)
+print(a * 2)
+print(a * 3)
+print(a * 4)
+print(a * 5)
+print(a * 6)
+print(a * 7)
+print(a * 8)
+print(a * 9)
+print = i
